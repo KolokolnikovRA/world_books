0537747cec3c4d69c7b56fa037092024f5232541
Roman Kolokolnikov <mr.ra_kolokolnikov@mail.ru>
2022-05-06 22:10:33 +0300

    Изменение django c 4 на 3.2.1. В новой версии изменение django.conf.urls на django.urls import re-path as url

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Book, Author, BookInstance, Genre
+from django.views import generic
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Главная страница сайта Мир книги!")
+    num_books = Book.objects.all().count()
+    num_instance = BookInstance.objects.all().count()
+    num_instance_available = BookInstance.objects.filter(status__exact=2).count()
+    num_author = Author.objects.count()
+
+    return render (request, 'index.html', context = {'num_books':num_books, 'num_instance':num_instance,
+                                            'num_instance_available':num_instance_available,
+                                            'num_author':num_author})
+
+class BookListView(generic.ListView):
+    model=Book
+
+class BookDetailView(generic.DetailView):
+    model = Book
